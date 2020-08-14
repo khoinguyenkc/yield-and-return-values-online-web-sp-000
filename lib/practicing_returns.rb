e1819dbec29a20a4a0d115e9a52786b47036b930
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-14 22:19:17 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -8,6 +8,11 @@ def hello(array)
     collection.push(yield(array[i]))
     i += 1
   end
+  collection
 end
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+puts hello(["Tim", "Tom", "Jim"]) { |name|  "Hi, #{name}" }
+#note this is not puts hi name, just hi name. puts hiname returns nothing. this returns a value
+#the vlue then gets saved to the collection array
+
+#we've build a function that does aexactly wht the collect method does
